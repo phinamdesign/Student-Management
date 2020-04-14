61ea70fb5c1960bb963ca5f9bc56b02c561c389e
Phi Nam <phinamhd1992@gmail.com>
2020-04-14 13:31:29 +0700

    join table

--- src/main/java/com/vsii/model/Student.java
+++ src/main/java/com/vsii/model/Student.java
@@ -1,9 +1,6 @@
 package com.vsii.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Student {
@@ -16,12 +13,47 @@ public class Student {
 
     private Long age;
 
+    @ManyToOne
+    @JoinColumn(name = "sexId")
+    private Sex sex;
+
+    @ManyToOne
+    @JoinColumn(name = "addressId")
+    private Address address;
+
+    @ManyToOne
+    @JoinColumn(name = "schoolId")
+    private School school;
+
+    @ManyToOne
+    @JoinColumn(name = "departmentId")
+    private Department department;
+
+    @ManyToOne
+    @JoinColumn(name = "classRId")
+    private ClassR classR;
+
+    @ManyToOne
+    @JoinColumn(name = "rankerId")
+    private Ranker ranker;
+
+    @ManyToOne
+    @JoinColumn(name = "conductId")
+    private Conduct conduct;
+
     public Student() {
     }
 
-    public Student(String name, Long age) {
+    public Student(String name, Long age, Sex sex, Address address, School school, Department department, ClassR classR, Ranker ranker, Conduct conduct) {
         this.name = name;
         this.age = age;
+        this.sex = sex;
+        this.address = address;
+        this.school = school;
+        this.department = department;
+        this.classR = classR;
+        this.ranker = ranker;
+        this.conduct = conduct;
     }
 
     public Long getId() {
@@ -46,5 +78,61 @@ public class Student {
 
     public void setAge(Long age) {
         this.age = age;
+    }
+
+    public Sex getSex() {
+        return sex;
+    }
+
+    public void setSex(Sex sex) {
+        this.sex = sex;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public School getSchool() {
+        return school;
+    }
+
+    public void setSchool(School school) {
+        this.school = school;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public ClassR getClassR() {
+        return classR;
+    }
+
+    public void setClassR(ClassR classR) {
+        this.classR = classR;
+    }
+
+    public Ranker getRanker() {
+        return ranker;
+    }
+
+    public void setRanker(Ranker ranker) {
+        this.ranker = ranker;
+    }
+
+    public Conduct getConduct() {
+        return conduct;
+    }
+
+    public void setConduct(Conduct conduct) {
+        this.conduct = conduct;
     }
 }
